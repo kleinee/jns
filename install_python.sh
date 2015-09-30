1c85b79ba6db71751aa07d126fe2339dcf164014
Eckhard Kleine <eckhard.kleine@gmail.com>
2015-09-30 17:46:13 +0700

    Create install_python.sh

---
+++ install_python.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# script name:     install_python.sh
+# last modified:   2015/09/30
+# sudo:            yes
+#
+# see: http://sowingseasons.com/blog/building-python-3-4-on-raspberry-pi-2.html
+
+if ! [ $(id -u) = 0 ]; then
+   echo "to be run with sudo"
+   exit 1
+fi
+
+#Python 3 version to install
+version="3.5.0"
+
+#------------------------------------------------------
+apt-get install -y build-essential libncursesw5-dev
+apt-get install -y libgdbm-dev libc6-dev
+apt-get install -y zlib1g-dev libsqlite3-dev tk-dev
+apt-get install -y libssl-dev openssl
+#------------------------------------------------------
+
+wget "https://www.python.org/ftp/python/$version/Python-$version.tgz"
+tar zxvf "Python-$version.tgz"
+cd "Python-$version"
+./configure
+make
+make install
+pip3 install pip --upgrade
+
+# soft link to make pip3 default
+ln -s /usr/local/bin/pip3 pip
+
+# clean up
+rm -rf "./Python-$version"
+rm "./Python-$version.tgz"
