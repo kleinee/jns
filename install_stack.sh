c73e382d9c76829382d5d6716958c60e44558efa
Eckhard Kleine <eckhard.kleine@gmail.com>
2015-09-30 17:55:16 +0700

    Create install_stack.sh

---
+++ install_stack.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# script name:     install_stack.sh
+# last modified:   2015/09/22
+# sudo:            yes
+
+if ! [ $(id -u) = 0 ]; then
+   echo "to be run with sudo"
+   exit 1
+fi
+
+pip install numpy
+pip install matplotlib
+pip install sympy
+pip install pandas
+pip install numexpr
+pip install bottleneck
+pip install SQLAlchemy
+pip install openpyxl
+pip install xlrd
+pip install xlwt
+pip install XlsxWriter
+pip install beautifulsoup4
+pip install html5lib
+
+#------------------------------------------------------
+apt-get -y install libxml2-dev libxslt-dev
+#------------------------------------------------------
+
+pip install lxml
+pip install requests
+pip install networkx
+
+#------------------------------------------------------
+apt-get -y install libatlas-base-dev gfortran
+#------------------------------------------------------
+
+pip install SciPy
+pip install statsmodels
+pip install -U scikit-learn
+pip install git+git://github.com/mwaskom/seaborn.git#egg=seaborn
