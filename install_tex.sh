433e0969fd68067060f78c4443b8f1c0783e7ad6
Eckhard Kleine <eckhard.kleine@gmail.com>
2015-09-30 17:49:18 +0700

    Create install_tex.sh

---
+++ install_tex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# script name:     install_tex.sh
+# last modified:   2015/09/22
+# sudo:            yes
+
+if ! [ $(id -u) = 0 ]; then
+   echo "to be run with sudo"
+   exit 1
+fi
+
+#------------------------------------------------------
+apt-get install -y texlive
+apt-get install -y texlive-latex-extra
+apt-get install -Y dvipng
+#------------------------------------------------------
