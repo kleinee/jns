c4c39b41ade92ca7f42ac586171d4fd5cc3b75b4
Eckhard Kleine <eckhard.kleine@gmail.com>
2015-09-30 17:51:20 +0700

    Create configure_jupyter.sh

---
+++ configure_jupyter.sh
@@ -0,0 +1,43 @@
+# script name:     configure_jupyter.sh
+# last modified:   2015/09/30
+# sudo:            no
+
+if [ $(id -u) = 0 ]
+then
+   echo "to be run as jns"
+   exit 1
+fi
+
+# generate config and create notebook directory
+jupyter notebook --generate-config
+cd $home
+mkdir notebooks
+
+target=~/.jupyter/jupyter_notebook_config.py
+
+# get current ip address - we assume it is static
+ip=$(echo $(hostname -I))
+
+# set up dictionary of changes for jupyter_config.py
+declare -A arr
+app='c.NotebookApp'
+arr+=(["$app.open_browser"]="$app.open_browser = False")
+arr+=(["$app.ip"]="$app.ip ='$ip'")
+arr+=(["$app.port"]="$app.port = 9090")
+arr+=(["$app.enable_mathjax"]="$app.enable_mathjax = True")
+arr+=(["$app.notebook_dir"]="$app.notebook_dir = '/home/jns/notebooks'")
+arr+=(["$app.password"]="$app.password = 'sha1:5815fb7ca805:f09ed218dfcc908acb3e29c3b697079fea37486a'")
+arr+=(["$app.server_extensions.append"]="$app.server_extensions.append('ipyparallel.nbextension')")
+
+# apply changes to jupyter_notebook_config.py
+
+# change or append
+for key in ${!arr[@]};do
+    if grep -qF $key ${target}; then
+        # key found -> replace line
+        sed -i "/${key}/c ${arr[${key}]}" $target
+    else
+        # key not found -> append line
+        echo "${arr[${key}]}" >> $target
+    fi
+done
