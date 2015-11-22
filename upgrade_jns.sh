3b05ae18cbad5cd69281e30a78320c047c261406
Eckhard Kleine <eckhard.kleine@gmail.com>
2015-11-22 10:44:04 +0700

    Update upgrade_jns.sh
    script now generates list of outdated packages and processes

--- upgrade_jns.sh
+++ upgrade_jns.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # script name:     upgrade_jns.sh
-# last modified:   2015/10/22
+# last modified:   2015/11/22
 # sudo:            yes
-
 
 if [ $(whoami) != 'root' ]; then
         echo "Must be root to run $0"
         exit 1;
 fi
 
-# adjust this list of packages you intend to upgrade
-list=(openpyxl pandas)
+# generate list of outdated packages
+echo ">>> CHECKING INSTALLATION FOR OUTDATED PACKAGES..."
+lst=(`pip list --outdated |grep -o '^\S*'`)
 
-for i in ${list[@]}; do
-    pip install ${i} --upgrade
-done
+# process list of outdated packages
+if [ ${#lst[@]} -eq 0 ]; then
+    echo ">>> INSTALLATION UP TO DATE"
+    exit 1;
+else
+    echo ">>> UPGRADING PACKAGES"
+    for i in ${lst[@]}; do
+        pip install ${i} --upgrade
+    done
+fi
