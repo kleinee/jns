2b254fd591466fdb9d042ffe5b7ef237e5a47786
Eckhard Kleine <eckhard.kleine@gmail.com>
2016-01-01 11:29:25 +0700

    Update upgrade_jns.sh
    Added rudimentary timer testing purposes.

--- upgrade_jns.sh
+++ upgrade_jns.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # script name:     upgrade_jns.sh
-# last modified:   2015/11/22
+# last modified:   2016/01/01
 # sudo:            yes
 
 if [ $(whoami) != 'root' ]; then
         echo "Must be root to run $0"
         exit 1;
 fi
+
+START=$SECONDS
 
 # generate list of outdated packages
 echo ">>> CHECKING INSTALLATION FOR OUTDATED PACKAGES..."
@@ -22,3 +24,6 @@ else
         pip install ${i} --upgrade
     done
 fi
+
+ELAPSED=$(($SECONDS - $START))
+echo $ELAPSED
