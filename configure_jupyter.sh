80f9277057660a53e77f3658d01fd9e7e88cd60d
Eckhard Kleine <eckhard.kleine@gmail.com>
2016-04-17 14:17:25 +0700

    Update configure_jupyter.sh
    Notebook 4.2 deprecates c.NotebookApp.server_extensions = []

--- configure_jupyter.sh
+++ configure_jupyter.sh
@@ -1,5 +1,5 @@
 # script name:     configure_jupyter.sh
-# last modified:   2015/09/30
+# last modified:   2016/04/17
 # sudo:            no
 
 if [ $(id -u) = 0 ]
@@ -27,7 +27,14 @@ arr+=(["$app.port"]="$app.port = 9090")
 arr+=(["$app.enable_mathjax"]="$app.enable_mathjax = True")
 arr+=(["$app.notebook_dir"]="$app.notebook_dir = '/home/jns/notebooks'")
 arr+=(["$app.password"]="$app.password = 'sha1:5815fb7ca805:f09ed218dfcc908acb3e29c3b697079fea37486a'")
-arr+=(["$app.server_extensions.append"]="$app.server_extensions.append('ipyparallel.nbextension')")
+
+# arr+=(["$app.server_extensions.append"]="$app.server_extensions.append('ipyparallel.nbextension')")
+
+# NOTE:
+# in Notebook 4.2 c.NotebookApp.server_extensions = [] is deprecated
+# 
+# you can run ipcluster nbextension enable but you will see 
+# messages about deprecation in the server log
 
 # apply changes to jupyter_notebook_config.py
 
