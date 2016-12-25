6c7585245906dc9720d2cb1e608f7aa82e0f5586
Eckhard Kleine <eckhard.kleine@gmail.com>
2016-12-25 18:23:58 +0700

    cleaned up

--- configure_jupyter.sh
+++ configure_jupyter.sh
@@ -25,17 +25,8 @@ arr+=(["$app.enable_mathjax"]="$app.enable_mathjax = True")
 arr+=(["$app.notebook_dir"]="$app.notebook_dir = '/home/jns/notebooks'")
 arr+=(["$app.password"]="$app.password = 'sha1:5815fb7ca805:f09ed218dfcc908acb3e29c3b697079fea37486a'")
 
-# arr+=(["$app.server_extensions.append"]="$app.server_extensions.append('ipyparallel.nbextension')")
-
-# NOTE:
-# in Notebook 4.2 c.NotebookApp.server_extensions = [] is deprecated
-# 
-# you can run ipcluster nbextension enable but you will see 
-# messages about deprecation in the server log
-
 # apply changes to jupyter_notebook_config.py
 
-# change or append
 for key in ${!arr[@]};do
     if grep -qF $key ${target}; then
         # key found -> replace line
