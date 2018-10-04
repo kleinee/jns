af8a05cc44c74f5c51d767b8d7c1b3fd0d18221c
kleinee <eckhard.kleine@gmail.com>
2018-10-04 18:29:47 +0700

    clean up...

--- scripts/update_requirements.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-me=`basename "$0"`
-pipdeptree -f --warn silence | grep -P '^[\w0-9\-=.]+' >requirements.txt
-#pipdeptree | grep -P '^\w+' >requirements.txt
-sed -i '/sqlite/d' ./requirements.txt
